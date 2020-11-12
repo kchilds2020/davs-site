fbf43698c57a5fc730dcc4fa848f42a5f7378456
kchilds2020 <63356648+kchilds2020@users.noreply.github.com>
2020-11-11 18:10:57 -0600

    fixed white background bug and separated slideshow with index

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -18,17 +18,6 @@ function SlideShow() {
                 
             </FDRow>
         </Header>
-        <Section> 
-            <SecInfo style={{backgroundColor: 'white'}}>
-            {/* <Logo src={picture}/> */}
-            </SecInfo>
-            <SecInfo>
-            <h1>About</h1>
-            <Paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac urna sapien. Vivamus tempor finibus felis, vel facilisis elit volutpat ac. Nulla nisl nisl, vestibulum sed ullamcorper id, placerat sit amet urna. Cras vitae orci justo. Maecenas non sapien accumsan, varius diam vitae, fringilla quam.
-            </Paragraph>
-            </SecInfo>
-        </Section>
         <SecDiv></SecDiv>
         </>
     )
@@ -38,9 +27,6 @@ const SecDiv = styled.div`
     height: 300px;
 `
 
-const Paragraph = styled.p`
-    font-size: 20px;
-`
 
 const Background = styled.img`
     width: 100%;
@@ -53,23 +39,6 @@ const Background = styled.img`
 `
 
 
-const Section = styled.div`
-    background-color: rgb(64, 72, 81);
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-evenly;
-    align-items: center;
-    padding: 20px;
-`
 
-const SecInfo = styled.div`
-    width: 500px;
-    height: 100%;
-    padding: 10px;
-    background-color: rgb(52, 58, 64);
-    color: white;
-    border-radius: 4px;
-    height: auto;
-`
 
 export default SlideShow
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import NavigationBar from '../components/NavigationBar'
 import SlideShow from '../components/SlideShow'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Helmet from 'react-helmet'
+import styled from 'styled-components'
 export default function Home() {
   return (
     <>
@@ -10,8 +11,43 @@ export default function Home() {
     <div style={{fontFamily: 'Raleway'}}>
       <NavigationBar/>
       <SlideShow />
+        <Section> 
+            <SecInfo style={{padding: '0px'}} >
+            {/* <Logo src={picture}/> */}
+            </SecInfo>
+            <SecInfo>
+            <h1>About</h1>
+            <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac urna sapien. Vivamus tempor finibus felis, vel facilisis elit volutpat ac. Nulla nisl nisl, vestibulum sed ullamcorper id, placerat sit amet urna. Cras vitae orci justo. Maecenas non sapien accumsan, varius diam vitae, fringilla quam.
+            </Paragraph>
+            </SecInfo>
+        </Section>
       <div style={{color: 'white', fontFamily: 'Raleway'}}>Hello world!!!</div>
     </div>
     </>
   )
 }
+
+
+const Section = styled.div`
+    background-color: rgb(64, 72, 81);
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    align-items: center;
+    padding: 20px;
+`
+
+const SecInfo = styled.div`
+    width: 500px;
+    height: 100%;
+    padding: 10px;
+    background-color: rgb(52, 58, 64);
+    color: white;
+    border-radius: 4px;
+    height: auto;
+`
+
+const Paragraph = styled.p`
+    font-size: 20px;
+`
